8d72e45d80c33de0d66cfb68f638b50c8543602f
ShubhamPal19 <77537775+ShubhamPal19@users.noreply.github.com>
2023-06-21 23:03:57 +0530

    Update AdminService.java

--- GMS_JEDI_2_Flipkart_Project_Java/src/com/flipkart/service/AdminService.java
+++ GMS_JEDI_2_Flipkart_Project_Java/src/com/flipkart/service/AdminService.java
@@ -3,19 +3,78 @@
  */
 package com.flipkart.service;
 
+import com.flipkart.bean.*;
+
+import java.util.*;
+
+
 /**
  * 
  */
 public class AdminService implements AdminGMSInterface{
+
+	
+	User user2 = new User("Tej", "s123", 1);
+	User user3 = new User("Lokesh", "s123", 1);
+
+
+	Registration register1 = new Registration("abc1", "abc", 1, "abc");
+	Registration register2 = new Registration("abc2", "abc", 1, "abc");
+	Registration register3 = new Registration("abc3", "abc", 1, "abc");
+
+	List<Registration> pendingRegistration  = new ArrayList<>(Arrays.asList(register1,register2,register3));
+
+	List<Registration> approved  = new ArrayList<>(Arrays.asList());
 	public void seePendingRequest() {
+
+		
+
+		System.out.println("List of pending registration :- ");
+		for(Registration val : pendingRegistration) {
+
+			System.out.println(val.getName()+" - regid: "+val.getCustomerId());
+
+		}
+
 		
 	}
 	
 	public boolean approveRequest(String requestId) {
+
+
+		
+		for (int i = 0; i < pendingRegistration.size(); i++) {
+			Registration register = pendingRegistration.get(i);
+		    if (register.getCustomerId().equals(requestId)) {
+
+		    	approved.add(register);
+
+		    	pendingRegistration.remove(i);
+		    	System.out.println("Registration with request id " + requestId + " is approved ");
+		    	break;
+
+		    }
+		}
+
+
+
+		System.out.println("The registration has been approved.");
 		return true;
 	}
 	
-	public boolean approveallRequest(String requestId) {
+	public boolean approveAllRequests() {
+
+
+		for (int i = 0; i < pendingRegistration.size(); i++) {
+			Registration register = pendingRegistration.get(i);
+
+		    approved.add(register);
+		 }
+
+		pendingRegistration.clear();
+
+		
+
 		return true;
 	}
 } 
